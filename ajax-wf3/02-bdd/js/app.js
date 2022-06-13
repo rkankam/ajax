92cef594ccc41dc345f12d117bd63f4b1751c3e7
rkankam <raoul.kankam@gmail.com>
2022-06-13 22:09:22 +0200

    Add files via upload
    looped render

--- ajax-wf3/02-bdd/js/app.js
+++ ajax-wf3/02-bdd/js/app.js
@@ -1,35 +1,21 @@
-function l (arg) {
-    console.log(arg);
+function l(arg) {
+	console.log(arg);
 }
 
-
-$(document).ready(function() {
-    $("#selectEmploye").change(function() {
-        // je stocke la value de l'option selectionnées dans une variable
-        let id_employe = $(this).val();
-        // l(id_employe);
-        $.ajax({
-            url: "recuperer_detail_employes.php",
-            type: "GET",
-            dataType: "json",
-            data: {monIdEmploye: id_employe}
-        }).done(function(data) {
-            
-            $("#id_employes").html(data.id_employes);
-            $("#prenom").html(data.prenom);
-            $("#nom").html(data.nom);
-            $("#sexe").html(data.sexe);
-            $("#service").html(data.service);
-            $("#date_embauche").html(data.date_embauche);
-            $("#salaire").html(data.salaire);
-
-            for (var i in data) {
-                l(data[i]);
-                $("")
-            }
-
-            
-        });
-    })
-})
-
+$(document).ready(function () {
+	$("#selectEmploye").change(function () {
+		// je stocke la value de l'option selectionnées dans une variable
+		let id_employe = $(this).val();
+		// l(id_employe);
+		$.ajax({
+			url: "recuperer_detail_employes.php",
+			type: "GET",
+			dataType: "json",
+			data: { monIdEmploye: id_employe },
+		}).done(function (data) {
+			for (var i in data) {
+				document.getElementById(i).innerHTML = data[i];
+			}
+		});
+	});
+});
